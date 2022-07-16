1ef2fcd488cb85ef9507c7703c343a7938f83eb3
CookK_S <vital2486@naver.com>
2022-07-16 16:24:48 +0900

    Decorate Main UI

---
+++ Assets/UI/Main Menu Crew/Scrips/CrewFloater.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewFloater : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private List<Sprite> sprites;
+
+    private bool[] crewStates = new bool[12];
+    private float timer = 0.5f;
+    private float distance = 11f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            SpawnFloatingCrew((EPlayerColor)i, Random.Range(0f, distance));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0f)
+        {
+            SpawnFloatingCrew((EPlayerColor)Random.Range(0, 12), distance);
+            timer = 1f;
+        }
+    }
+
+    public void SpawnFloatingCrew(EPlayerColor playerColor, float dist)
+    {
+        if(!crewStates[(int)playerColor])
+        {
+            crewStates[(int)playerColor] = true;
+
+            float angle = Random.Range(0f, 360f);
+            Vector3 spawnPos = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0f) * distance;
+            Vector3 direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 1f);
+            float floatingSpeed = Random.Range(1f, 4f);
+            float rotateSpeed = Random.Range(-3f, 3f);
+
+            var crew = Instantiate(prefab, spawnPos, Quaternion.identity).GetComponent<FloatingCrew>();
+            crew.SetFloatingCrew(sprites[Random.Range(0, sprites.Count)], playerColor, direction, floatingSpeed, rotateSpeed, Random.Range(0.5f, 1f));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var crew = collision.GetComponent<FloatingCrew>();
+        if(crew != null)
+        {
+            crewStates[(int)crew.playerColor] = false;
+            Destroy(crew.gameObject);
+        }
+    }
+
+}
